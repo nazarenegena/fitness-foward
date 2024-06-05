9da4cc12aeed938e1fe44ebcb19490bd2ec087c0
Nazarene Gena <genarene96@gmail.com>
2024-06-05 16:38:17 +0300

    removed unused imports

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import React from "react";
 import { useForm } from "react-hook-form";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 import { SelectedPage } from "@/shared/types";
